a2ff1088117b9a01a5362e060c88681b16d1ad25
Destaby <rudik.nick.nick@gmail.com>
2021-02-23 17:34:26 +0200

    Initialize project structure

---
+++ db/install.sql
@@ -0,0 +1,2 @@
+CREATE USER usertest WITH PASSWORD 'password';
+CREATE DATABASE findyouranimal OWNER usertest;
---
+++ server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const { Worker } = require('worker_threads');
+const path = require('path');
+
+const Config = require('./lib/config.js');
+
+const PATH = process.cwd();
+const CFG_PATH = path.join(PATH, 'config');
+
+const options = { trackUnmanagedFds: true };
+
+(async () => {
+  const config = await new Config(CFG_PATH);
+  const count = config.sections.server.ports.length;
+  const workers = new Array(count);
+
+  const start = id => {
+    const worker = new Worker('./lib/worker.js', options);
+    workers[id] = worker;
+    worker.on('exit', code => {
+      if (code !== 0) start(id);
+    });
+  };
+
+  for (let id = 0; id < count; id++) start(id);
+
+  const stop = async () => {
+    for (const worker of workers) {
+      worker.postMessage({ name: 'stop' });
+    }
+  };
+
+  process.on('SIGINT', stop);
+  process.on('SIGTERM', stop);
+})();
