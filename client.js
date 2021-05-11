9102261289cd27662a05401a3c50d75fc4800765
Destaby <rudik.nick.nick@gmail.com>
2021-05-11 13:01:19 +0300

    add log sql

--- client.js
+++ client.js
@@ -3,21 +3,20 @@ const fs = require('fs');
 const path = require('path');
 
 const pgclient = new Client({
-    host: '127.0.0.1',
-    port: 5432,
-    user: 'usertest',
-    password: 'password',
-    database: 'findyouranimal'
+  host: '127.0.0.1',
+  port: 5432,
+  user: 'usertest',
+  password: 'password',
+  database: 'findyouranimal',
 });
 
 pgclient.connect();
 
 fs.readFile(path.join(__dirname, 'db/structure.sql'), 'utf8', (err, data) => {
   if (err) throw err;
+  console.log(data);
   pgclient.query(data, (err, res) => {
-    if (err) throw err
+    if (err) throw err;
     process.exit(0);
   });
 });
-
-// comments
